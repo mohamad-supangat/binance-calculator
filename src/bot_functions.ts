5269312b3188e98726bcb48d0d7667190a5e4fc9
Mohamad Supangat <hantamkoding@gmail.com>
2022-10-09 20:05:33 +0700

    calculator.ts

--- src/bot_functions.ts
+++ src/bot_functions.ts
@@ -1,18 +1,55 @@
+function imr(leverage: float) {
+  return 1 / leverage;
+}
+
+function inital_margin(qty: float, entry_price: float, imr: float) {
+  return qty * entry_price * imr;
+}
+
+/**
+ * Menghitung data keuntungan
+ *
+ * @param type - -1 untuk short dan 1 untuk long
+ * @param exit_price - harga jual / close posisi
+ * @param entry_price - harga entry / open posisi
+ * @param qty - quantity order
+ */
+function pnl(type: int, exit_price: float, entry_price: float, qty: float) {
+  if (type == 1) {
+    return (exit_price - entry_price) * qty;
+  } else {
+    return (entry_price - exit_price) * qty;
+  }
+}
+
+/**
+ * Menghitung hasil ROE% berapa perssen laba / rugi
+ *
+ * @param pnl - hasil dari fungsi pnl
+ * @param inital_margin - hasil dari fungsi inital_margin
+ * @returns
+ */
+function roe(pnl: float, inital_margin: float) {
+  return pnl / inital_margin;
+}
+
+function target_price(type: int, entry_price: float, roe) {}
+
 function generate_qty_order({ order_amount, price, leverage }) {
   let qty = (Number.parseFloat(order_amount) / price) * leverage;
 
   return qty;
 }
 
-function generate_target_price({ target_profit, type, price, qty }) {
+function generate_target_price({ target_profit, type, open_price, qty }) {
   let target_price;
   if (type == "buy") {
     target_price =
-      Number.parseFloat(price) +
+      Number.parseFloat(open_price) +
       Number.parseFloat(target_profit) / Number.parseFloat(qty);
   } else {
     target_price =
-      Number.parseFloat(price) -
+      Number.parseFloat(open_price) -
       Number.parseFloat(target_profit) / Number.parseFloat(qty);
   }
 
---
+++ src/calculator.ts
@@ -0,0 +1,56 @@
+export class Calculator {
+  type: number;
+  leverage: number;
+  entry_price: number;
+  balance_amount: number;
+  roe: number;
+
+  constructor(
+    type: number,
+    leverage: number,
+    entry_price: number,
+    balance_amount: number,
+    roe: number
+  ) {
+    this.type = type;
+    this.leverage = leverage;
+    this.entry_price = entry_price;
+    this.balance_amount = balance_amount;
+    this.roe = roe;
+  }
+
+  qty(): number {
+    return (this.balance_amount / this.entry_price) * this.leverage;
+  }
+
+  imr(): number {
+    return 1 / this.leverage;
+  }
+
+  inital_margin(): number {
+    return this.qty() * this.entry_price * this.imr();
+  }
+
+  // roe() {
+  //   return this.pnl() / this.inital_margin();
+  // }
+
+  exit_price(): number {
+    if (this.type == 1) {
+      return this.entry_price * (this.roe / this.leverage + 1);
+    } else {
+      return this.entry_price * (1 - this.roe / this.leverage);
+    }
+  }
+
+  /**
+   * Menghitung data keuntungan
+   */
+  pnl(): number {
+    if (this.type == 1) {
+      return (this.exit_price() - this.entry_price) * this.qty();
+    } else {
+      return (this.entry_price - this.exit_price()) * this.qty();
+    }
+  }
+}
